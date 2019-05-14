389220057e00b210e009935f7a7aac3b229770ee
Shawn Ren <shawnren527@gmail.com>
2019-05-14 19:42:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  let form = document.getElementById('create-task-form')
-  let input = form.querySelector('input')
-  let submit = document.querySelectorAll('form')[1]
-  let ul = document.getElementById('tasks')
-  let li = document.createElement('li')
-  let btn = document.createElement('button')
-  btn.innerHTML = 'X'
-  li.appendChild(btn)
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+//
+// });
+let form = document.getElementById('create-task-form')
+let input = form.querySelector('input')
+let submit = document.querySelectorAll('form')[1]
+let ul = document.getElementById('tasks')
+let li = document.createElement('li')
+let btn = document.createElement('button')
+btn.innerHTML = 'X'
+li.appendChild(btn)
 
-  submit.addEventListener('click', function(e){
-    btn.setAttribute('data-description', `${input.value}`)
-    ul.appendChild(li)
-  })
-});
+submit.addEventListener('click', function(e){
+  btn.setAttribute('data-description', `${input.value}`)
+  ul.appendChild(li)
+})
